cf9fe08510126c22ea977c70a89bad12aaf86213
dbmurray <darragh.murray@gmail.com>
2021-07-13 21:26:23 +1000

    Revert "Updates to config"
    This reverts commit bfabbbbe892834b96435968ce9bac1d22acb7e45.

--- scripts/start_site_new_blog.R
+++
@@ -1,9 +0,0 @@
-library(blogdown)
-serve_site() 
-
-blogdown::build_dir("static")
-
-blogdown::new_post(title = "The Land is a Map", 
-                   ext = '.Rmarkdown', 
-                   subdir = "post")
-blogdown::check_site()
